7a861ff53cc5b1e77ad54a4e7c4a0d201d6ab5bb
EvanJameson <ecjameson@gmail.com>
2016-11-04 12:48:41 -0700

    added empty test files and HashTools file

---
+++ lab09/Evan/HashTools.java
@@ -0,0 +1,33 @@
+/**
+ *Hash tools class
+ *
+ *
+ *
+ *@author Evan Jameson
+ *@version Lab 09
+ */
+
+import java.util.*;
+
+public class HashTools
+{
+   public static <T> double avgCollisions(List<T> list, int tableSize, Hashable<T> hashable)
+   {
+
+   }
+
+   public static <T> int collisions(List<T> list, int tableSize, Hashable<T> hashable)
+   {
+
+   }
+
+   public static <T> int maxCollisions(List<T> list, int tableSize, Hashable<T> hashable)
+   {
+
+   }
+
+   public static <T> int unUsed(List<T> list, int tableSize, Hashable<T> hashable)
+   {
+
+   }
+}
