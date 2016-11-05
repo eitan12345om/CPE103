ea20104bbb11d0474551005cea7630892ba6b029
Eitan Simler <eitan.simler@gmail.com>
2016-11-05 16:38:04 -0700

    Add class skeletons

---
+++ project4/Eitan/MorseToText.java
@@ -0,0 +1,25 @@
+/**
+ * A class for converting Morse Code into text.
+ *
+ * @author Eitan Simler
+ * @version Project4
+ */
+
+public class MorseToText implements BSTTranslator<MorseOrder> {
+
+   // Constructor
+   public MorseToText() {
+      // TODO: Add constructor body
+   }
+
+   // Methods
+   public BST<MorseOrder> getBST() {
+      // TODO: Add method body
+      return null;
+   }
+
+   public String translate(String s) {
+      // TODO: Add method body
+      return "";
+   }
+}
---
+++ project4/Eitan/TextToMorse.java
@@ -0,0 +1,25 @@
+/**
+ * A class for converting Morse Code into text.
+ *
+ * @author Eitan Simler
+ * @version Project4
+ */
+
+public class TextToMorse implements BSTTranslator<CharacterOrder> {
+
+   // Constructor
+   public TextToMorse() {
+      // TODO: Add constructor body
+   }
+
+   // Methods
+   public BST<CharacterOrder> getBST() {
+      // TODO: Add method body
+      return null;
+   }
+
+   public String translate(String s) {
+      // TODO: Add method body
+      return "";
+   }
+}
