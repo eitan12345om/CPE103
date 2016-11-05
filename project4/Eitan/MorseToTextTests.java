f92e2f1c059a299e8970e5405d613562f68b61c4
Eitan Simler <eitan.simler@gmail.com>
2016-11-05 16:45:44 -0700

    Added Test class skeletons

---
+++ project4/Eitan/MorseToTextTests.java
@@ -0,0 +1,15 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * JUnit tests for MorseToText.
+ *
+ * @author Eitan Simler
+ * @version Project4
+ */
+
+public class MorseToTextTests {
+
+
+}
+
---
+++ project4/Eitan/TextToMorseTests.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * JUnit tests for TextToMorse.
+ *
+ * @author Eitan Simler
+ * @version Project4
+ */
+
+public class TextToMorseTests {
+
+
+}
