b368f4ae772aa11aabe270af8a8592c3aee193a3
Eitan Simler <eitan.simler@gmail.com>
2016-11-06 14:07:13 -0800

    Initial Commit

---
+++ lab09/Eitan/StringHashTests.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * JUnit tests for StringHash.
+ *
+ * @author Eitan Simler
+ * @version Project1
+ */
+
+public class StringHashTests {
+
+   // TODO: Add tests
+}
