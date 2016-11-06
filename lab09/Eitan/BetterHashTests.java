570bf9bc969eda9ad41d81e4b26027f4298ec505
Eitan Simler <eitan.simler@gmail.com>
2016-11-06 14:17:49 -0800

    Initial Commit

---
+++ lab09/Eitan/BetterHashTests.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * JUnit tests for BetterHash.
+ *
+ * @author Eitan Simler
+ * @version Project1
+ */
+
+public class BetterHashTests {
+
+   // TODO: Add tests
+}
