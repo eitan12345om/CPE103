5c6ebda244bc3573dc9a74bb1c3aa356cfbab5a9
Eitan Simler <eitan.simler@gmail.com>
2016-11-06 20:08:47 -0800

    Initial Commit. Still need tests

---
+++ lab09/Eitan/MyHashTests.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * JUnit tests for MyHash.
+ *
+ * @author Eitan Simler
+ * @version Lab09
+ */
+
+public class MyHashTests {
+
+   // TODO: Add tests
+}
