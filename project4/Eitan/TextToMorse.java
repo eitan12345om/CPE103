5daf50557119f52ba9148b861ce74e3f1a9b00a9
Eitan Simler <eitan.simler@gmail.com>
2016-11-08 17:48:16 -0800

    Finished Constructor

--- project4/Eitan/TextToMorse.java
+++ project4/Eitan/TextToMorse.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * A class for converting Morse Code into text.
  *
@@ -7,19 +9,46 @@
 
 public class TextToMorse implements BSTTranslator<CharacterOrder> {
 
+   // Instance variables
+   private BST<CharacterOrder> tree;
+   private ArrayList<CharacterOrder> arr;
+
    // Constructor
    public TextToMorse() {
-      // TODO: Add constructor body
+      tree = new BST<>();
+      arr = new ArrayList<>(MorseCode.size());
+      preSorting();
+      tree.toSortedList(arr);
+      constructBST(0, MorseCode.size() / 2, MorseCode.size() - 1);
    }
 
    // Methods
    public BST<CharacterOrder> getBST() {
-      // TODO: Add method body
-      return null;
+      return tree;
    }
 
    public String translate(String s) {
       // TODO: Add method body
       return "";
    }
-}
+
+
+
+   // Private helper methods
+   private void preSorting() {
+      for (int i = 0; i < MorseCode.size(); i++) {
+         tree.insert(new CharacterOrder(MorseCode.get(i)));
+      }
+   }
+   
+   private void constructBST(int low, int mid, int high) {
+      // Recursive binary search like thing
+      tree.insert(arr.get(mid));
+      if (low < mid) {
+         constructBST(low, (low + mid) / 2, mid - 1);
+      }
+      if (high > mid) {
+         constructBST(mid + 1, (high + mid + 1) / 2, high);
+      }
+   }
+}   
