139836a5ec2ede9406714b0c846fe544d52ca53a
Eitan Simler <eitan.simler@gmail.com>
2016-11-22 21:01:51 -0800

    Added general shell sort method

--- lab11/Eitan/ShellSorts.java
+++ lab11/Eitan/ShellSorts.java
@@ -22,4 +22,27 @@ public class ShellSorts {
    public static <T extends Comparable<? super T>> void heapSort(T[] array) {
       // TODO: Method body
    }
+
+   private static <T extends Comparable<? super T>> void shellSort(T[] array,
+         int[] gaps) {
+      T temp;
+      int j;
+      
+      // For each gap
+      for (int gap : gaps) {
+         // Loop through array
+         for (int i = 0; i < array.length; i++) {
+            temp = array[i];
+
+            // Shift elements until correct location for array[i] is found
+            for (j = i; j >= gap && array[j - gap].compareTo(temp) > 0; 
+                  j -= gap) {
+               array[j] = array[j - gap];
+            }
+
+            // Put temp in correct location
+            array[j] = temp;
+         }
+      }
+   }
 }
