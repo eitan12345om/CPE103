310c68d8711896b9741455d1f73c6dde6c857df5
Eitan Simler <eitan.simler@gmail.com>
2016-11-22 20:25:29 -0800

    Added skeleton of shell sorts class

---
+++ lab11/Eitan/ShellSorts.java
@@ -0,0 +1,25 @@
+/**
+ * A class containing multiple implementations of Shell Sort.
+ *
+ * @author Eitan Simler
+ * @version Lab11
+ */
+
+public class ShellSorts {
+
+   public static <T extends Comparable<? super T>> void shell(T[] array) {
+      // TODO: Method body
+   }
+   
+   public static <T extends Comparable<? super T>> void hibbard(T[] array) {
+      // TODO: Method body
+   }
+   
+   public static <T extends Comparable<? super T>> void sedgewick(T[] array) {
+      // TODO: Method body
+   }
+   
+   public static <T extends Comparable<? super T>> void heapSort(T[] array) {
+      // TODO: Method body
+   }
+}
