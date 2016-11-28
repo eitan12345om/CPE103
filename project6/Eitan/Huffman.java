b5ab74dc5c5cb570e40b260f276267ab70ee1295
Eitan Simler <eitan.simler@gmail.com>
2016-11-28 13:53:50 -0800

    Added skeleton for Huffman class

---
+++ project6/Eitan/Huffman.java
@@ -0,0 +1,30 @@
+/**
+ * Performs encoding and decoding with the Huffman algorithm.
+ *
+ * @author Eitan Simler
+ * @version Project6
+ */
+
+public class Huffman {
+
+   // Constructor
+   public Huffman(String fileName) {
+   
+   }
+
+   // Methods
+   public String decode(String encoded) {
+      // TODO: Add method body
+      return ""; 
+   }
+
+   public String encode(String text) {
+      // TODO: Add method body
+      return "";  
+   }
+
+   public String toString() {
+      // TODO: Add method body
+      return "";
+   }
+}
