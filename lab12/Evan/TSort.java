4b64cbd5b13d46b6df9841cd63b8672e31e90ed8
EvanJameson <ecjameson@gmail.com>
2016-12-02 23:54:12 -0800

    code needs comments, also poor variable choice

--- lab12/Evan/TSort.java
+++ lab12/Evan/TSort.java
@@ -2,7 +2,7 @@
  * Provided starting point for Java-based tsort.
  *
  * @author Hatalsky/Jones - Starting point.
- * @author ? - Completed by.
+ * @author Evan Jameson - Completed by.
  *
  * @version CPE 103 Lab 12
  */
@@ -34,9 +34,119 @@ public class TSort {
     *   contains a loop"</li>
     * </ul>
     */
-   public static String tsort(String edges) {
-      return "TODO by you!";
+
+
+
+   //-----------------------------------------------------------------------------
+   public static String tsort(String edges) 
+   {
+      Scanner s = new Scanner(edges);
+      if(!s.hasNext())
+      {
+         throw new IllegalArgumentException("input contains no edges");
+      }
+      else if(oddVert(edges))
+      {
+         throw new IllegalArgumentException("input contains an odd number of tokens");
+      }
+      else
+      {
+         ArrayList<Vertex> vertecies = new ArrayList<>();
+        // Scanner s = new Scanner(edges);
+         while(s.hasNext())
+         {
+            String check = s.next();
+            if(vertecies.size() != 0)
+            {
+               boolean fudge = true;
+               while(fudge)
+               {
+
+                  for(Vertex vert: vertecies)
+                  {
+                     if(check.equals(vert.name))
+                     {
+                        fudge = false; 
+                     }
+                  }
+                  if(fudge)
+                  {
+                     Vertex v = new Vertex(check);//only gets here if not yet seen
+                     vertecies.add(v);
+                  }
+                  fudge = false;
+               }
+            }
+            if(vertecies.size() == 0)
+            {
+               Vertex v = new Vertex(check);
+               vertecies.add(v);
+            }
+         }
+         s.close();
+
+         //now we have a list of the unique verticies
+         for(Vertex vert: vertecies)
+         {
+            //System.out.print(vert.name + " " );  
+         }
+
+         Scanner last= new Scanner(edges);
+         while(last.hasNext())
+         {
+            String check1 = last.next();
+            String check2 = last.next();
+            int i = 0;
+            int j = 0;
+            for(Vertex vert: vertecies)
+            {
+               if(check1.equals(vert.name))
+               {
+                  i = vertecies.indexOf(vert);
+               }
+               if(check2.equals(vert.name))
+               {
+                  j = vertecies.indexOf(vert);
+               }
+            }
+            buildAdj(vertecies.get(i), vertecies.get(j));
+         }
+         last.close();
+
+         //now we have list of unique vertex objects and they all contain their aadjacencies
+         
+         Stack<Vertex> stack = new Stack<>();
+         StringBuilder output = new StringBuilder();
+         for(Vertex vert: vertecies)
+         {
+            if(vert.inDegree == 0)
+            {
+               stack.push(vert);
+            }
+         }
+         while(!stack.empty())
+         {
+            Vertex v = stack.pop();
+            output.append(v.name + "\n");
+            for(Vertex vert: v.adj)
+            {
+               vert.inDegree -= 1 ;
+               if(vert.inDegree == 0)
+               {
+                  stack.push(vert);
+               }
+            }
+         }
+
+
+      return output.toString();
+      }
    }
+   //use a hash map to store stuff but also keep an arraylist of the verticies to keep them in order
+   //-----------------------------------------------------------------------------
+   
+   
+
 
    /**
     * Entry point for the Java-based tsort utility allowing the user to specify
@@ -73,6 +183,56 @@ public class TSort {
       } catch(IllegalArgumentException e) {
          System.out.print("TSort: " + e.getMessage());
          System.exit(1);
+      }
+   }
+   //-----------------------------------------------------------------------------
+   //private helper methods
+ 
+   //returns true if the edges has an odd number of vertecies
+   private static boolean oddVert(String edges)
+   {
+      int num = 0;
+      Scanner scan = new Scanner(edges);
+      while(scan.hasNext())
+      {
+         num += 1;
+         scan.next();
+      }
+      scan.close();
+      if(num%2 == 1)
+      {
+         return true;
+      }
+      return false;
+   }
+
+   //
+   private static void buildAdj(Vertex vert1,Vertex vert2)
+   {
+      
+      for(Vertex vert: vert2.adj)
+      {
+         if(vert1.name.equals(vert.name))
+         {
+           throw new IllegalArgumentException("input contains a loop");
+         }
+
+      }
+      vert1.adj.add(vert2);
+      vert2.inDegree += 1;
+      
+   }
+
+   //-----------------------------------------------------------------------------
+   private static class Vertex
+   {
+      ArrayList<Vertex> adj = new ArrayList<>(); //list of adjacent vertecies
+      int inDegree = 0;     //in degree #
+      String name;
+
+      public Vertex(String name) 
+      {
+         this.name = name;
       }
    }
 }
