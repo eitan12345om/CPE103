1512a0d5968b80b34455ba14c5969f73bc5df03b
Eitan Simler <eitan.simler@gmail.com>
2016-12-04 22:46:53 -0800

    Added tests for PQ and FileNotFoundException

---
+++ project6/Eitan/HuffmanTests.java
@@ -0,0 +1,31 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.io.*;
+
+/**
+ * JUnit tests for Huffman.
+ *
+ * @author Eitan Simler
+ * @version Project6
+ */
+
+public class HuffmanTests {
+
+   @Test (expected = FileNotFoundException.class)
+   public void test_FileNotFound() throws FileNotFoundException, IOException {
+      Huffman huff = new Huffman("hello.txt");
+   }
+
+   @Test
+   public void test_ArrayList() throws FileNotFoundException, IOException {
+      Huffman huff = new Huffman("textfile.txt");
+      huff.printPQ();
+   }
+   
+   @Test
+   public void test_ArrayList2() throws FileNotFoundException, IOException {
+      Huffman huff = new Huffman("textfile2.txt");
+      huff.printPQ();
+   }
+}
