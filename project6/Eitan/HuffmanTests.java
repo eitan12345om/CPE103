e5d505bde209c2ba52bd5a7cf0b0364b12e731b7
Eitan Simler <eitan.simler@gmail.com>
2016-12-07 10:21:14 -0800

    Added tests

--- project6/Eitan/HuffmanTests.java
+++ project6/Eitan/HuffmanTests.java
@@ -2,6 +2,7 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 import java.io.*;
+import java.util.*;
 
 /**
  * JUnit tests for Huffman.
@@ -33,23 +34,43 @@ public class HuffmanTests {
    public void test_decompress() throws FileNotFoundException, IOException {
       Huffman huff = new Huffman("textfile.txt");
       huff.decompress("compressed.txt", "decompressed.txt");
+      
+      Scanner scan1 = new Scanner(new File("decompressed.txt"));
+      Scanner scan2 = new Scanner(new File("textfile.txt"));
+      
+      assertEquals(scan2.nextLine(), scan1.nextLine());
    }
 
    @Test
    public void test_decompress2() throws FileNotFoundException, IOException {
       Huffman huff = new Huffman("textfile2.txt");
       huff.decompress("compressed2.txt", "decompressed2.txt");
+      
+      Scanner scan1 = new Scanner(new File("decompressed2.txt"));
+      Scanner scan2 = new Scanner(new File("textfile2.txt"));
+      
+      assertEquals(scan2.nextLine(), scan1.nextLine());
    }
    
-   //@Test
-   //public void test_compress() throws FileNotFoundException, IOException {
-   //   Huffman huff = new Huffman("textfile.txt");
-   //   huff.compress("textfile.txt", "textfile_compressed.txt");
-   //}
+   @Test
+   public void test_compress() throws FileNotFoundException, IOException {
+      Huffman huff = new Huffman("textfile.txt");
+      huff.compress("textfile.txt", "textfile_compressed.txt");
+      
+      Scanner scan1 = new Scanner(new File("textfile_compressed.txt"));
+      Scanner scan2 = new Scanner(new File("compressed.txt"));
+      
+      assertEquals(scan2.nextLine(), scan1.nextLine());
+   }
 
    @Test
    public void test_compress2() throws FileNotFoundException, IOException {
       Huffman huff = new Huffman("textfile2.txt");
       huff.compress("textfile2.txt", "textfile_compressed2.txt");
+      
+      Scanner scan1 = new Scanner(new File("textfile_compressed2.txt"));
+      Scanner scan2 = new Scanner(new File("compressed2.txt"));
+      
+      assertEquals(scan2.nextLine(), scan1.nextLine());
    }
 }
